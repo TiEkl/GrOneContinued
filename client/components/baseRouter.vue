21709ab715dc0451c0f7b7c33e6e3a4539c57e52
Lucas Fredin <ii.fredin@gmail.com>
2018-11-16 10:52:38 +0100

    added main.js file (we only need one since there are no different types of users, added comments of what to add in vue files

---
+++ client/components/baseRouter.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <!-- base router (if we want a header or footer etc) -->
+        <!-- header -->
+
+        <!-- footer -->
+        
+    </div>
+</template>
+
+
+<script>
+  module.exports = {
+    name:"BaseRouter"
+  };
+</script>
+
+
+<style>
+
+</style>
+
--- client/components/inputScreen.vue
+++ client/components/inputScreen.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <!-- here is the main screen the user sees when the go on the page (input field n such) -->
+        <!-- Header and explanation of the product -->
+        <!-- input field and button (hide after user presses btn, it was an ok url, and now results are loading) -->
+        <!-- hidden loading screen that only shows up
+            when the user has submitted by pressing the btn (dont allow user to submit some invalid url) -->
+        <!-- replace with another component when viewing the result -->
     </div>
 </template>
 
