691c8cd74c89278e893f2196387820b21454a799
Lucas Fredin <ii.fredin@gmail.com>
2018-11-16 10:21:15 +0100

    "Skeleton for the input page, and the base router"

---
+++ client/components/inputScreen.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <!-- here is the main screen the user sees when the go on the page (input field n such) -->
+    </div>
+</template>
+
+
+<script>
+  module.exports = {
+    name:"InputScreen"
+  };
+</script>
+
+
+<style>
+
+</style>
+
