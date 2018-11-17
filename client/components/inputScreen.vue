68f4b860b9cb9810563831ce85a3331bee1d3869
Lucas Fredin <ii.fredin@gmail.com>
2018-11-17 10:12:32 +0100

    worked on base for inputScreen vue

--- client/components/inputScreen.vue
+++ client/components/inputScreen.vue
@@ -6,18 +6,65 @@
         <!-- hidden loading screen that only shows up
             when the user has submitted by pressing the btn (dont allow user to submit some invalid url) -->
         <!-- replace with another component when viewing the result -->
+
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
+
+            <div v-if="url_accepted===false">   
+                <form>
+                    <label for="url_input_form">Please enter a valid GitHub project url</label>
+                    <input id="url_input_form" class="form-control" type ="url" v-model="Url_Input.url" required pattern="https?://.+">
+                    <button class="btn btn-info" type="button" @click="save_url()">Create visualization</button>
+                </form>    
+            </div>
+
+            <div v-if="url_accepted===true">
+                <div id="showProgress">
+                    <div id="progressBar"></div>
+                </div>
+            </div>
+           
+        </div>
+        <div class="col-sm-3"></div>
+
     </div>
 </template>
 
 
 <script>
-  module.exports = {
-    name:"InputScreen"
-  };
+    var axios = require('axios');
+    module.exports = {
+        name:"InputScreen",
+        data(){
+            return{
+                Url_Input:{
+                    url:''
+                },
+                url_accepted: false
+            }
+        },
+        methods:{
+            save_url(){
+                 axios.post('http://localhost:3000/api/urls', new_url)
+                .then((response)=>{
+
+                    url_accepted = true;
+                })
+                .catch((error)=>{
+                    console.log(error.response);  
+                });
+            }
+        },
+        mounted(){
+
+        }
+    };
 </script>
 
 
 <style>
+
+
 
 </style>
 
