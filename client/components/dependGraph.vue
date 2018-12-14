3e959d385a6e7b1d21d1298983fdfc50b5e36825
guseklti <guseklti@student.gu.se>
2018-12-14 19:54:40 +0100

    Colours nodes by the package name

--- client/components/dependGraph.vue
+++ client/components/dependGraph.vue
@@ -56,53 +56,69 @@ import * as d3 from 'd3';
         const nodes = data.nodes.map(d => Object.create(d));
         const simulation = this.forceSimulation(nodes, links).on("tick", ticked);
         const scale = d3.scaleOrdinal(d3.schemeCategory10);
+        const scale1_10 = d3.scaleOrdinal(d3.schemeSet1);
+        const scale10_17 = d3.scaleOrdinal(d3.schemeSet2);
+        const scale18_29 = d3.scaleOrdinal(d3.schemeSet3);
         
         const svg = d3.select(".frame").append("svg")
             .attr("viewBox", [-this.width / 2, -this.height /2, this.width, this.height]);
 
+        var stringToColour = str => {
+            var hash = 0;
+            for (var i = 0; i < str.length; i++) {
+                hash = str.charCodeAt(i) + ((hash << 5) - hash);
+            }
+            var colour = '#';
+            for (var i = 0; i < 3; i++) {
+                var value = (hash >> (i * 8)) & 0xFF;
+                colour += ('00' + value.toString(16)).substr(-2);
+            }
+            return colour;
+        }
+
         var text = svg.append("g").selectAll("text")
-            .data(nodes)
-            .enter().append("text")
-            .attr("class", "text")
-            .attr("opacity", 0)
-            .attr("x", 20)
-            .attr("y", ".31em")
-            .text(function(d) { return d.id; })
+                .data(nodes)
+                .enter().append("text")
+                .attr("class", "text")
+                .attr("opacity", 0)
+                .attr("x", 20)
+                .attr("y", ".31em")
+                .text(function(d) { return d.id; });
         
         const link = svg.append("g")
             .attr("stroke", "#999")
             .attr("stroke-opacity", 0)
-            .selectAll("line")
-            .data(links)
-            .enter().append("line")
+          .selectAll("line")
+          .data(links)
+          .enter().append("line")
             .attr("stroke-width", d => Math.sqrt(d.value));
+            
         const node = svg.append("g")
             .attr("stroke", "#fff")
             .attr("stroke-width", 3)
-            .selectAll("circle")
-            .data(nodes)
-            .enter().append("circle")
-            .attr("class", "circle")
+          .selectAll("circle")
+          .data(nodes)
+          .enter().append("circle")
+          .attr("class", "circle")
             .attr("r", 5)
 
-            .attr("fill",  d => scale(d.group))
+            .attr("fill",  d => stringToColour(d.package))
             .call(drag(simulation))
             .on("mouseover", mouseOver(.2))
-            .on("mouseout", mouseOut)
+        .on("mouseout", mouseOut)
         
- 
+
         function ticked() {
-            link
-                .attr("x1", d => d.source.x)
-                .attr("y1", d => d.source.y)
-                .attr("x2", d => d.target.x)
-                .attr("y2", d => d.target.y);
+          link
+              .attr("x1", d => d.source.x)
+              .attr("y1", d => d.source.y)
+              .attr("x2", d => d.target.x)
+              .attr("y2", d => d.target.y);
           
-            node
-                .attr("cx", d => d.x)
-                .attr("cy", d => d.y);
-            text
-                .attr("transform", transform);
+          node
+              .attr("cx", d => d.x)
+              .attr("cy", d => d.y);
+            text.attr("transform", transform);
         }
 
         function transform(d) {
@@ -114,7 +130,7 @@ import * as d3 from 'd3';
             linkedByIndex[d.source.index + "," + d.target.index] = 1;
         });
         function isConnected(a, b) {
-        return linkedByIndex[a.index + "," + b.index] || linkedByIndex[b.index + "," + a.index] || a.index == b.index;
+            return linkedByIndex[a.index + "," + b.index] || linkedByIndex[b.index + "," + a.index] || a.index == b.index;
         }
     // fade nodes on hover
         function mouseOver(opacity) {
@@ -164,12 +180,9 @@ import * as d3 from 'd3';
             .force("charge", d3.forceManyBody())
             .force("collide", d3.forceCollide().radius(25))
             .force("center", d3.forceCenter());
-      },
-
+      }
     },
   mounted() {
-
-    
      var drag = simulation => {
       
       function dragstarted(d) {
@@ -198,12 +211,12 @@ import * as d3 from 'd3';
         return d => scale(d.group);
     }
    
-    d3.json("/api/dependencies")
-        .then( data =>  {
-        console.log(JSON.stringify(data));
-        this.drawChart(data, drag);
+  d3.json("/api/dependencies")
+    .then( data =>  {
+      console.log(JSON.stringify(data));
+      this.drawChart(data, drag, color);
     });
-    console.log("Test");
+    
   },
 };
 </script>
