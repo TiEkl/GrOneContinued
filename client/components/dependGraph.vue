afcba6cb5b7b3d199dd7125f54377d3a89ec73e6
guseklti <guseklti@student.gu.se>
2018-12-14 18:14:27 +0100

    added build script to package.json. Removed double api call in dependGraph.vue

--- client/components/dependGraph.vue
+++ client/components/dependGraph.vue
@@ -60,101 +60,102 @@ import * as d3 from 'd3';
         const svg = d3.select(".frame").append("svg")
             .attr("viewBox", [-this.width / 2, -this.height /2, this.width, this.height]);
 
-   var text = svg.append("g").selectAll("text")
-        .data(nodes)
-        .enter().append("text")
-        .attr("class", "text")
-        .attr("opacity", 0)
-        .attr("x", 20)
-        .attr("y", ".31em")
-        .text(function(d) { return d.id; })
+        var text = svg.append("g").selectAll("text")
+            .data(nodes)
+            .enter().append("text")
+            .attr("class", "text")
+            .attr("opacity", 0)
+            .attr("x", 20)
+            .attr("y", ".31em")
+            .text(function(d) { return d.id; })
         
         const link = svg.append("g")
             .attr("stroke", "#999")
             .attr("stroke-opacity", 0)
-          .selectAll("line")
-          .data(links)
-          .enter().append("line")
+            .selectAll("line")
+            .data(links)
+            .enter().append("line")
             .attr("stroke-width", d => Math.sqrt(d.value));
         const node = svg.append("g")
             .attr("stroke", "#fff")
             .attr("stroke-width", 3)
-          .selectAll("circle")
-          .data(nodes)
-          .enter().append("circle")
-          .attr("class", "circle")
+            .selectAll("circle")
+            .data(nodes)
+            .enter().append("circle")
+            .attr("class", "circle")
             .attr("r", 5)
 
             .attr("fill",  d => scale(d.group))
             .call(drag(simulation))
             .on("mouseover", mouseOver(.2))
-        .on("mouseout", mouseOut)
+            .on("mouseout", mouseOut)
         
  
         function ticked() {
-          link
-              .attr("x1", d => d.source.x)
-              .attr("y1", d => d.source.y)
-              .attr("x2", d => d.target.x)
-              .attr("y2", d => d.target.y);
+            link
+                .attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
           
-          node
-              .attr("cx", d => d.x)
-              .attr("cy", d => d.y);
-            text.attr("transform", transform);
+            node
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+            text
+                .attr("transform", transform);
         }
 
         function transform(d) {
-  return "translate(" + d.x + "," + d.y + ")";
-}
+            return "translate(" + d.x + "," + d.y + ")";
+        }
 
-    var linkedByIndex = {};
-    links.forEach(function(d) {
-        linkedByIndex[d.source.index + "," + d.target.index] = 1;
-    });
+        var linkedByIndex = {};
+        links.forEach(function(d) {
+            linkedByIndex[d.source.index + "," + d.target.index] = 1;
+        });
         function isConnected(a, b) {
         return linkedByIndex[a.index + "," + b.index] || linkedByIndex[b.index + "," + a.index] || a.index == b.index;
-    }
+        }
     // fade nodes on hover
-    function mouseOver(opacity) {
-        return function(d) {
-            // check all other nodes to see if they're connected
-            // to this one. if so, keep the opacity at 1, otherwise
-            // fade
-            // also style link accordingly
-            node.style("stroke-opacity", function(o) {
+        function mouseOver(opacity) {
+            return function(d) {
+                // check all other nodes to see if they're connected
+                // to this one. if so, keep the opacity at 1, otherwise
+                // fade
+                // also style link accordingly
+                node.style("stroke-opacity", function(o) {
+                    var thisOpacity = isConnected(d, o) ? 1 : opacity;
+                    return thisOpacity;})
+                    .transition().duration(1000);;
+                node.style("fill-opacity", function(o) {
                 var thisOpacity = isConnected(d, o) ? 1 : opacity;
-                return thisOpacity;})
-                .transition().duration(1000);;
-            node.style("fill-opacity", function(o) {
-               var thisOpacity = isConnected(d, o) ? 1 : opacity;
-                return thisOpacity;})
-                .transition().duration(1000);
-            link.style("stroke-opacity", function(o) {
-                return o.source === d || o.target === d ? 1 : opacity;
-            }).transition().duration(1000);
-            link.style("stroke", function(o){
-                return o.source === d || o.target === d ? o.source.colour : "#fff";
-            }).transition().duration(1000);
+                    return thisOpacity;})
+                    .transition().duration(1000);
+                link.style("stroke-opacity", function(o) {
+                    return o.source === d || o.target === d ? 1 : opacity;
+                }).transition().duration(1000);
+                link.style("stroke", function(o){
+                    return o.source === d || o.target === d ? o.source.colour : "#fff";
+                }).transition().duration(1000);
+                
+                text.style("opacity", function(o) {
+                    var thisOpacity = isConnected(d, o) ? 1 : opacity;
+                    return thisOpacity;})
+                    .transition().duration(1000);;
+                text.style("fill-opacity", function(o) {
+                var thisOpacity = isConnected(d, o) ? 1 : opacity;
+                    return thisOpacity;})
+            };
+        }
+        function mouseOut() {
+            node.style("stroke-opacity", 1).transition().duration(1000);
+            node.style("fill-opacity", 1).transition().duration(1000);
+            link.style("stroke-opacity", 0).transition().duration(1000);
+            link.style("stroke", "#ddd").transition().duration(1000);
+            text.style("opacity", 0).transition().duration(1000);
             
-            text.style("opacity", function(o) {
-                var thisOpacity = isConnected(d, o) ? 1 : opacity;
-                return thisOpacity;})
-                .transition().duration(1000);;
-            text.style("fill-opacity", function(o) {
-               var thisOpacity = isConnected(d, o) ? 1 : opacity;
-                return thisOpacity;})
-        };
-    }
-    function mouseOut() {
-        node.style("stroke-opacity", 1).transition().duration(1000);
-        node.style("fill-opacity", 1).transition().duration(1000);
-        link.style("stroke-opacity", 0).transition().duration(1000);
-        link.style("stroke", "#ddd").transition().duration(1000);
-        text.style("opacity", 0).transition().duration(1000);
-        
-        
-    }
+            
+        }
         return svg.node();
       },
       forceSimulation : function(nodes, links) {
@@ -164,12 +165,11 @@ import * as d3 from 'd3';
             .force("collide", d3.forceCollide().radius(25))
             .force("center", d3.forceCenter());
       },
-      setData : function() {
-        this.data = d3.json("/api/dependencies");        
-      },
+
     },
   mounted() {
-    var test = this.setData();
+
+    
      var drag = simulation => {
       
       function dragstarted(d) {
@@ -198,12 +198,12 @@ import * as d3 from 'd3';
         return d => scale(d.group);
     }
    
-  d3.json("/api/dependencies")
-    .then( data =>  {
-      console.log(JSON.stringify(data));
-      this.drawChart(data, drag, color);
+    d3.json("/api/dependencies")
+        .then( data =>  {
+        console.log(JSON.stringify(data));
+        this.drawChart(data, drag);
     });
-    
+    console.log("Test");
   },
 };
 </script>
