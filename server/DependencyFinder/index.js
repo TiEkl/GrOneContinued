e44e6fdf7f23ae7f16794d76643bb8134c6f4cbd
guseklti <guseklti@student.gu.se>
2018-12-14 14:22:58 +0100

    Removed extra get request for dependencies in dependGraph

--- server/DependencyFinder/index.js
+++ server/DependencyFinder/index.js
@@ -4,11 +4,44 @@ var router = express.Router();
 var xml2js = require('xml2js');
 const fs = require('fs');
 const perf = require('execution-time')();
+// lol gigigle
+
+
+//gets to actual repository using the url
+//router.use('/api/repos', require('./repofetcher/repos.js'))
+
+/**********************TARGET SERVER*************/
+// target server would have the different ways to handle the request. 
+// eventually this should be in the different api/ files
+router.get('/app1/',function(req,res) {
+    console.log("server 1 sends its regards")
+    res.send("Hello world From Server 1");
+});
+/*************************************************/
+/*router.route('/').get(function (req, res) { //??
+    console.log("test");
+    res.sendFile(req.app.get('appPath') + '/index.html');
+    
+});*/
+
+// Insert routes below
+//router.use('/api/camels', require('./camels'));
+
+// All other routes redirect to the index.html
+// router.route('/owner').get(function (req, res) {
+//     res.sendfile(req.app.get('appPath') + '/owner.html');
+// });
+
+// router.route('/buyer').get(function (req, res) {
+//     res.sendfile(req.app.get('appPath') + '/buyer.html');
+// });
 
 router.route('/').get(function (req, res) {
     var relativeAppPath = req.app.get('appPath');
     var absoluteAppPath = path.resolve(relativeAppPath);
     res.sendFile(absoluteAppPath +'/index.html')
+    //res.sendFile('../../client/index.html');
+    console.log("path:" + absoluteAppPath);
 });
 
 router.get('/api', function(req, res) {
@@ -22,6 +55,7 @@ router.route('/api/dependencies').get(function(req,res) {
         });
 
     })
+
 })
 //Function for finding dependencies with an xml file as input and a callback function
 //that should handle the result from the function
@@ -34,7 +68,8 @@ function findDependencies(xml, callback) {
         var object = result.unit.unit;  //each .java file in json
 
         var graphData = { 
-            "nodes":[], "links":[] };
+            "nodes":[], 
+            "links":[] };
         regexSearch(object);
         
         function regexSearch(object) {
@@ -44,16 +79,20 @@ function findDependencies(xml, callback) {
             //For loop that creates each Node for the graphData, checks for classes and interfaces.
             //Also stringifies each class/interface to prepare for the regex matching.
             for (var i = 0; i < object.length; i++) {
-                var currentNode = {"id": "", "group": 1, "count": 0};
+                var currentNode = {"id": "", "package": "","group": 1, "count": 0};
                 if (object[i].class != null) {      //check if the java file includes any class
                     var currentName = object[i].class[0].name;
+                    var currentPackage = object[i].package[0].name;
                     currentNode.id = currentName.toString();
+                    currentNode.package = currentPackage.toString();
                     stringsJson[i] = JSON.stringify(object[i].class); //object[i].class is the current class in java file at [i] .
 
                 }
                 else if (object[i].interface != null) {         //check if the java file includes any interface
                     var currentName = object[i].interface[0].name;
+                    var currentPackage = object[i].package[0].name;
                     currentNode.id = currentName.toString();
+                    currentNode.package = currentPackage.toString();
                     stringsJson[i] = JSON.stringify(object[i].interface);
                 }
                 graphData.nodes.push(currentNode);
