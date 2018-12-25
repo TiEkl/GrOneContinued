dec22879284b7296ad79beb464b4f2e7769c1b07
Lucas Fredin <ii.fredin@gmail.com>
2018-12-25 11:21:31 +0100

    added a new method that works better than the last one (so we can basically get rid of the old one now), got help from lunchbadger.com (will send link), still a lot of problems tho, im making a list of problems

--- server/loadBalancer/balanceController.js
+++ server/loadBalancer/balanceController.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var cors = require('cors');
 var request = require('request');
-var app = express();
+var app = express().post('*', balanceLoad).get('*', balanceLoad);
 var roundround = require('roundround');
 //var router = express.Router();
 
@@ -29,22 +29,9 @@ var serverips = [bbServer2withPort,bbServer1withPort];
 // implement a algo to select server 
 var ips = roundround(serverips);
 
-app.use(cors());
+var current = 0;
 
-//Check if the servers are up and runnning before we attempt to connect to them
-//performs a handshake with the server and returns true if the server responded and is up
-function checkAvailability(){
-    (async ()=>{
-        console.log('check if blackboard1 is up: ' + await isReachable(bbServer2withPort));
-        console.log('check if blackboard2 is up: ' + await isReachable(bbServer1withPort));
-        if(await isReachable(bbServer1withPort)){
-            console.log('its all true! well 1 atleast');
-        }
-        if(!await isReachable(bbServer2withPort)){
-            console.log('its all false! well 1 atleast');
-        }
-    })();
-}
+app.use(cors());
 
 function proxyRequestTo (ip,endpoint){
     
@@ -62,31 +49,71 @@ function proxyRequestTo (ip,endpoint){
                 let url = 'http://'+ip + endpoint;
                 console.log('reroute to: ' + url);
                 req.pipe(request(url)).pipe(res);
+                current = (current+1) % serverips.length;
                 
             }
             else{ //if the first server we tried is down we try again.
+                current = (current+1) % serverips.length;
                 console.log(await isReachable(ip));
                 console.log('first server tried was not online, try again with recursive call');
-                ip = ips();
+                ip = serverips[current];
                 console.log(ip); //should be the next ip in the array
                 return proxyRequestTo(ip,endpoint);
             }
         })();
     });
-    
+}
 
- 
+//this is very similar to the other function, but for this function the recursive call works 
+//(which it didnt for the other one since it was set as a middleware)
+//so with this one is a BB is down it will not try to connect to it. and it will use the recursive call to try to find it again
+//for some reason there is still the problem that I think the method is sending the same request twice for some reason
+function balanceLoad(req,res){
+    console.log('WHERE we SEND the stuff '+serverips[current] + req.url);
+    var http = 'http://';
+    (async ()=>{
+        if( !await isReachable(bbServer1withPort) && !await isReachable(bbServer2withPort) ){ //these should be if(false and false)
+            //both servers are down, nothing we can do but wait for them to go back up and redo request
+            console.log('ALL SERVERS OFFLINE');
+        }
+        else if(await isReachable(serverips[current])){
+            console.log('The first server tried was online');
+            req.pipe(request({url: http + serverips[current] + req.url})).pipe(res); //will not work now since we are not just calling /api/gitprojects in the front end!!
+            current = (current+1) % serverips.length;
+        }
+        else{ //a bit hardcoded so should change this bit!
+            console.log('The first server tried was NOT online, try NEXT');
+            current = (current+1) % serverips.length;
+            return balanceLoad(req,res);
+        }
+    })();
+    
+}
+
+//Check if the servers are up and runnning before we attempt to connect to them
+//performs a handshake with the server and returns true if the server responded and is up
+function checkAvailability(){
+    (async ()=>{
+        console.log('check if blackboard1 is up: ' + await isReachable(bbServer2withPort));
+        console.log('check if blackboard2 is up: ' + await isReachable(bbServer1withPort));
+        if(await isReachable(bbServer1withPort)){
+            console.log('its all true! well 1 atleast');
+        }
+        if(!await isReachable(bbServer2withPort)){
+            console.log('its all false! well 1 atleast');
+        }
+    })();
 }
 
 checkAvailability();
-proxyRequestTo(ips(),'/api/gitProjects'); 
+//proxyRequestTo(serverips[current],'/api/gitProjects'); 
 
 
 
 ///All the stuff an app.js needs. (Not sure if we need all of them).
 //Now we can run balanceController.js on port 8002 so that we can make a request from the front end to port 8002
 //and the balancer will reroute it
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb', extended: true}));
 // HTTP request logger
 app.use(morgan('dev'));
 // Serve static assets (for frontend client)
